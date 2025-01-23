d80691b82e9ae17f7043ee15982f6d742c92a95d
Anjan <anjansuman80@gmail.com>
2025-01-23 23:42:28 +0530

    made generalised buttons with variable use.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,8 @@
 
 import './App.css'
 import { Button } from "./Components/ui/Button";
+import { PlusIcon } from './icons/PlusIcon';
+import { ShareIcon } from './icons/shareIcon';
 
 function App() {
 
@@ -8,8 +10,9 @@ function App() {
   }
 
   return (
-    <div className='h-80 w-80 bg-blue-300 '>
-      <Button variant={'primary'} size={'md'} text={'this good'} onClick={colorHandler} />
+    <div className=''>
+      <Button startIcon={<ShareIcon size={'md'}/>} variant={'primary'} size={'md'} text={'share'} onClick={colorHandler} />
+      <Button startIcon={<PlusIcon size={'md'}/>} variant={'secondary'} size={'md'} text={'Add content'} onClick={colorHandler} />
     </div>
   )
 }
--- Frontend/src/Components/ui/Button.tsx
+++ Frontend/src/Components/ui/Button.tsx
@@ -1,13 +1,27 @@
+import { ReactElement } from "react";
 
 export interface ButtonProps {
     variant: "primary" | "secondary";
     size: "sm" | "md" | "lg";
     text: string;
     startIcon?: any;
-    endIcon?: any;
+    endIcon?: ReactElement;
     onClick: () => void;
 }
 
+const variantStyles = {
+    "primary": "bg-black text-white",
+    "secondary": "bg-gray-300 text-black"
+}
+
+const defaultStyles = "rounded-md flex";
+
+const sizeStyles = {
+    "sm": "text-sm px-2 py-1",
+    "md": "text-md px-4 py-2",
+    "lg": "text-xl px-6 py-2"
+}
+
 export const Button = (props: ButtonProps) => {
-    return <button onClick={props.onClick} className={`h-${props.size} w-${props.size} bg-${props.variant}`}>{props.startIcon}{props.text}{props.endIcon}</button>
+    return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}>{props.startIcon} {props.text} {props.endIcon}</button>
 }
